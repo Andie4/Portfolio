ac335ffb3ff17cd0640e594ff42a0b32e3be3207
Andréa.C <155273473+Andie4@users.noreply.github.com>
2024-12-30 22:01:17 +0100

    retouche design accueil

--- srcipt.js
+++ srcipt.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ----------------------------------slider -------------------------------
 
-const slide = ["https://images.pexels.com/photos/2466778/pexels-photo-2466778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "https://images.pexels.com/photos/21972308/pexels-photo-21972308/free-photo-of-femme-nuit-rue-sombre.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "https://images.pexels.com/photos/2774556/pexels-photo-2774556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"];
+const slide = ["./images/img1.png", "./images/img3.png", "./images/img4.png"];
 let numero = 0;
 
 function ChangeSlide(sens) {
